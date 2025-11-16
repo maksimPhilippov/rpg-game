30b87e8a0f1c928d63a19fd9316e907288cfba75
Maksim Filippov <maksimphilippov@yahoo.com>
2025-11-16 20:44:44 +0400

    SDL transparent window

--- main.cxx
+++ main.cxx
@@ -1,6 +1,31 @@
-#include <iostream>
 
-int main() {
-    std::cout << "Hello, world!" << std::endl;
-    return 0;
+#define SDL_MAIN_USE_CALLBACKS
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
+    const int windowHeight = 100;
+    const int windowWidth = 100;
+
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        return SDL_APP_FAILURE;
+    }
+
+    if (!SDL_CreateWindow("РПГ гаме", windowWidth, windowHeight, SDL_WINDOW_RESIZABLE | SDL_WINDOW_TRANSPARENT | SDL_WINDOW_FULLSCREEN)) {
+        return SDL_APP_FAILURE;
+    }
+
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void *appstate) {
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
+    return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void *appstate, SDL_AppResult result) {
+    return;
 }
